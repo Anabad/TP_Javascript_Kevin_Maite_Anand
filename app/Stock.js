0fa2f01b1b0bf238386656815556c81d6b9a793d
DidelotK <Didelot_k@esme.fr>
2016-03-20 21:15:48 +0100

    MISE a jour

--- app/Stock.js
+++ app/Stock.js
@@ -14,16 +14,37 @@ class Stock {
         this.afficherStock();
     }
 
-    __initialiserIngredient(){
+    __initialiserIngredient() {
         var ingredient = creationTableauVide(NOMBRE_INGREDIENT);
         for (var i = 0; i < ingredient.length; i++) {
             ingredient[i] = STOCK_DEPART;
         }
         return ingredient;
     }
-    afficherStock(){
+
+    __ravitaillement() {
+
+    }
+    /**             RESTE INGREDIENT
+     * Ici le fonction reste ingredient va tester plusieurs choses en fonction de ce qu'on lui donnera
+     *   Si:
+     *       - null alors la fonction testera toute les ingrédients pour voir si il ne sont pas en dessous du
+     *           seuil critique
+     *       - un indice alors la fonction testera simplement un seul ingrédient
+     *           Il faudra donner en option : "Indice"
+     *       - une recette alors la fonction regardera si il reste assez d'ingrédient pour la recette
+     *           Il faudra donner en option : "Recette"
+     **/
+    __resteIngredient(test,option){
+
+    }
+    __retirerIngredients(){
+        
+    }
+    afficherStock() {
         for (var i = 0; i < this.ingredient.length; i++) console.log(this.ingredient[i]);
     }
+
 }
 
 // Petit test
