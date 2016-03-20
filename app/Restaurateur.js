2c5e28b5bf3a4f509ba21926e611988ab3d6f4a9
admin <cruciforme>
2016-03-20 19:48:26 +0100

    mise a jour

---
+++ app/Restaurateur.js
@@ -0,0 +1,23 @@
+'use strict'
+
+
+class Restaurateur {
+
+    constructor(horaire) {
+        this.nombreRecette = getRandom(5, 10);
+        this.horaireRestaurateur = horaire;
+
+
+    }
+
+    rentreRestaurant() {
+        if (this.horaireRestaurateur.estOuvert(heure)) {
+
+        }
+    }
+
+    ravitaille() {
+        tempsRavitaillement = getRandom(15, 115);
+    }
+}
+
---
+++ app/Stock.js
@@ -0,0 +1,19 @@
+'use strict'
+
+class Stock {
+
+    constructor() {
+        for (i < 0; i < NBINGREDIENTS; i++) {
+            ingredient
+                [i] = getRandom(20, 30);
+            max
+                [i] = ingredient[i];
+        }
+    }
+
+    ravitaille() {
+        for (i < 0; i < NBINGREDIENTS; i++) {
+            ingredient[i] = max[i];
+        }
+    }
+}
