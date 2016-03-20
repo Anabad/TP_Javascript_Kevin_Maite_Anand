0d1647f0c456c12e538fd8643900a4d5cb26822a
DidelotK <Didelot_k@esme.fr>
2016-03-20 21:54:03 +0100

    Mise à jour

--- app/Stock.js
+++ app/Stock.js
@@ -15,7 +15,8 @@ class Stock {
         this.afficherStock();
     }
 
-    /** INITIALISER INGREDIENT
+    /**
+     *  INITIALISER INGREDIENT
      *
      *  Cette fonction va initialiser le stock en ingredient lors de se création
      * @returns {*}
@@ -34,7 +35,8 @@ class Stock {
     }
 
     /**
-     *  /**             RESTE INGREDIENT
+     *  RESTE INGREDIENT
+     *
      * Ici le fonction reste ingredient va tester plusieurs choses en fonction de ce qu'on lui donnera
      *   Si:
      *       - null alors la fonction testera tous les ingrédients pour voir si ils ne sont pas en dessous du
@@ -52,7 +54,7 @@ class Stock {
     __resteIngredient(test, option) {
         if (test == null) {
             for (var i = 0; i < this.ingredient.length; i++) {
-                if (!this.__resteIngredientIndice(i,SEUIL_CRITIQUE)) {
+                if (!this.__resteIngredientIndice(i, SEUIL_CRITIQUE)) {
                     return false;
                 }
             }
@@ -68,7 +70,8 @@ class Stock {
         }
     }
 
-    /** RESTE INGREDIENT INDICE
+    /**
+     *  RESTE INGREDIENT INDICE
      *
      *  Cette fonction retourne vrai si le nombre d'ingrédient donné par l'indice est supérieur à la valeur
      *  donné
@@ -78,32 +81,36 @@ class Stock {
      * @returns {boolean}
      * @private
      */
-    __resteIngredientIndice(indice,valeur) {
+    __resteIngredientIndice(indice, valeur) {
         if (indice <= valeur) {
             return false;
         }
         return true;
     }
 
-    /** RETIRER INGREDIENTS
+    /**
+     *  RETIRER INGREDIENTS
      *
      *  Cette fonction retire les ingrédients nécessaires dans le stock pour faire une recette, elle retourne
      *  true si tout s'est bien passé false si il y n'y avait pas assez d'ingrédient.
      *  Puis elle testera si il reste assez d'ingrédient pour le futur, si non, elle lancera un ravitaillement
      *
+     *  C'est la fonction que le restaurant utilisera pour retirer des ingrédients
+     *
      * @param recette
      * @returns {boolean}
      * @private
      */
-    __retirerIngredients(recette) {
-        if(!this.__resteIngredient(recette, "Recette")) return false;
+    retirerIngredients(recette) {
+        if (!this.__resteIngredient(recette, "Recette")) return false;
 
         for (var i = 0; i < this.ingredient.length; i++) this.ingredient[i] -= recette[i];
         return true;
-        if(!this.__resteIngredient(null, null)) this.__ravitaillement();
+        if (!this.__resteIngredient(null, null)) this.__ravitaillement();
     }
 
-    /** AFFICHER STOCK
+    /**
+     *  AFFICHER STOCK
      *
      *  Cette fonction affiche le stock restant de chaque ingrédient
      */
